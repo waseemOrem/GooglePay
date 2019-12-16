b585be40e4e4b37fc9ae90bacb95b3b3844f3a41
admin <admin@rajeshs-macbook-pro.local>
2019-12-16 12:26:50 +0530

    Initial Commit

---
+++ GooglePay/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GooglePay
+//
+//  Created by admin on 16/12/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
